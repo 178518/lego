498d31a4322e3a4ddc0cd200b1dad0ea01084db6
云易 <yunyi.zy@alibaba-inc.com>
2016-03-28 21:29:21 +0800

    增加动画

--- src/js/comm-list.js
+++ src/js/comm-list.js
@@ -6,7 +6,7 @@ import CommList from './components/comm-list';
 //import CommList from 'rc-lego/comm-list';
 import Pubsub from 'pubsub-js';
 
-/*var itemAjaxConfig = {
+var itemAjaxConfig = {
   url: 'https://api.weibo.com/2/statuses/public_timeline.json',
   params: {
     source: 2043051649
@@ -21,38 +21,9 @@ var itemSuccessDataConfig = {
 };
 
 var itemDataConfig = {
-  id: 'id',
+  id: ['id'],
   subject: ['text'],
   imgUrl: ['user', 'profile_image_url'],
-  textClickCallBack:function(event){
-    console.log(event);
-  }
-};*/
-
-var itemAjaxConfig = {
-  url: 'http://m.aliexpress.com/column/pushedRecommendAjax.do',
-  params: {
-    page:1,
-    pageSize:10,
-    subjectLine:0,
-    imageSize:'350x350',
-    _currency:'USD',
-    visitorId:'1794e364-20fb-45e8-8354-3089e9bb54c6',
-    streamId:'9f0035e0-45b8-4d50-b8b0-d2be33dd2ad4'
-  },
-  dataType: 'jsonp',
-  type: 'get',
-  timeout: 5000
-};
-
-var itemSuccessDataConfig = {
-  itemList: ['productList']
-};
-
-var itemDataConfig = {
-  id: ['productId'],
-  subject: ['promotionMinAmountString'],
-  imgUrl: ['imgUrl'],
   textClickCallBack:function(event){
     console.log(event);
   }
